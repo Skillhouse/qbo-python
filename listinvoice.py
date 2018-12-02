82d5d7e3cae8257ed32cb4365b54bfcd42edc82d
Allen S. Rout <asr@routhouse.org>
2018-12-02 18:43:03 -0500

    Nice and simple, show me some invoices.

---
+++ listinvoice.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python 
+
+import requests
+import json
+import hackerspace_utils as hu
+
+authbag = hu.get_auth_bag()
+
+
+url = "https://sandbox-quickbooks.api.intuit.com/v3/company/123146047051614/query"
+
+querystring = {"minorversion":"14"}
+
+payload="select * from invoice startposition 1 maxresults 5"
+
+Token = authbag['token'];
+
+headers = {
+    'User-Agent': "QBOV3-OAuth2-Postman-Collection",
+    'Accept': "application/json",
+    'Content-Type': "application/text",
+    'Authorization': "Bearer "+Token,
+    'Cache-Control': "no-cache",
+}
+
+response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+
+resj = json.loads(response.text);
+qresult = resj['QueryResponse']
+invoices = qresult['Invoice']
+
+# import pdb; pdb.set_trace()
+
+distillate = { inv['Id']:
+               {
+                   'custid':inv['CustomerRef']['value'],
+                   'custname':inv['CustomerRef']['name'],
+                   'TxnDate':inv['TxnDate'],
+                   'Amount':inv['TotalAmt'],
+               }
+               for inv in invoices }
+
+print (json.dumps(distillate,sort_keys=True,indent=4))
+
+
+
+
+
+
+
+

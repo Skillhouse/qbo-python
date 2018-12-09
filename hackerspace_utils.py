56c661d6455ec8a57952a3177acf701714b8c630
Allen S. Rout <asr@routhouse.org>
2018-12-09 10:58:47 -0500

    Another data store.

--- hackerspace_utils.py
+++ hackerspace_utils.py
@@ -4,6 +4,13 @@ import json
 import re;
 
 
+# What accounts do payments get recorded into?
+payment_config="./payment_processing.json"
+
+def get_paymentconfig():
+    with open(payment_config) as cfgfile:
+        return json.load(cfgfile)
+
 
 # Out of the repo, natch.
 secrets="../hackerspace-auth.json"

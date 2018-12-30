9a5d7caa3ca656c96626b90caeb45a60f19bafa7
Allen S. Rout <asr@routhouse.org>
2018-12-30 01:33:25 -0500

    MOre detail, and split the auth into a long term and a cache.

--- hackerspace_utils.py
+++ hackerspace_utils.py
@@ -19,10 +19,56 @@ def get_paymentconfig():
 secrets="../hackerspace-auth.json"
 
 def get_auth_bag():
+    bag1 = ""
     with open(secrets) as authfile:
-        return json.load(authfile)
+        bag1 = json.load(authfile)
+
+    cache = get_bearer_cache()
+
+    bag1.update(cache)
+
+    return(bag1)
+    
 
 
+cache="../bearer-cache.json"
+
+def get_bearer_cache(fail_ood=True):
+    tempcache = ""
+    with open(cache) as infile:
+        tempcache= json.load(infile)
+
+
+    secs_still_good = tempcache['token_expires'] - time.time()
+
+    if(debug): print("# QBO auth token from cache good for {0:.1f} secs".format(secs_still_good))
+
+    if(secs_still_good < 1 and fail_ood is True):
+        raise ValueError('Token from cache no longer valid; expired {0}'.format(tempcache['token_expires_h']))
+    
+    return(tempcache)
+
+
+def dump_bearer_cache(bearin,cacheout=cache):
+    with open(cacheout,"w") as outfile:
+        os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+        time.tzset()
+        json.dump(
+            {
+                'token' : bearin.accessToken,
+                'refresh_token': bearin.refreshToken,
+                'token_expires':   time.time() +bearin.accessTokenExpiry ,
+                'refresh_expires': time.time() +bearin.refreshExpiry ,
+                'token_expires_h':   time.ctime(time.time() +bearin.accessTokenExpiry) ,
+                'refresh_expires_h': time.ctime(time.time() +bearin.refreshExpiry) ,
+
+            },
+            outfile,
+            indent=4,
+            sort_keys=True,
+            )
+   
+    
 stripepat = re.compile(r"^stripe: *(?P<custid>.+)$",re.MULTILINE)
     
 def extract_stripecust_from_notes(innotes):

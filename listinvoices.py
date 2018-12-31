d404baa898d32ce28ee9f1bfd35117f83f3728cc
Allen S. Rout <asr@routhouse.org>
2018-12-30 23:22:38 -0500

    Fuller featured invoice list, parameters and deletion option.

---
+++ listinvoices.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python 
+"""listinvoices -- 
+
+List invoices from QBO.
+
+
+
+Usage:
+  listinvoices.py  [options]
+
+Options:
+  --debug                  Print debugging output. [default: False]
+
+  --starting=<DATE>        Invoices on or after this date. [default: the first]
+  --ending=<DATE>          Invoices before (NOT on) this date. [default: today]
+
+  --custid <id[,id...]>    List for specified QBO IDs. 
+
+  --and-then-delete        Remove the invoices revealed by this query.
+
+  --doit                   Actually do one of the optional things.  Otherwise, is a no-op. [default: False]
+
+
+"""
+
+
+from docopt import docopt
+import requests
+import json
+import hackerspace_utils as hu
+import pandas as pd;
+import sys
+import time
+import stripe;
+from datetime import date
+import dateparser
+
+from quickbooks import Oauth2SessionManager
+from quickbooks import QuickBooks
+from quickbooks.objects.customer import Customer
+from quickbooks.objects import Account
+from quickbooks.objects import EmailAddress
+from quickbooks.objects import Invoice
+from quickbooks.batch import batch_delete
+
+
+
+import qbo_utils as qu
+
+#
+# Still thinking about how to parameterize this.  While it's clumsy,
+# it'll stay broken out here.
+#
+
+qbo_client = None
+
+def invoice_iterable(whereclause="",**kwargs):
+    reps = 0
+    yielded = 0
+    
+    while (True):
+        
+        invoices = Invoice.where(whereclause,
+            qb=qbo_client,
+            max_results=100,
+            start_position=yielded+1,
+            **kwargs
+        )
+        reps += 1
+
+        if len(invoices) == 0:
+            return
+
+        for item in invoices:
+
+            yielded += 1
+            yield(item)
+
+
+
+def invoice2row(inv):
+    bag = {
+        'TxnDate'   : inv.TxnDate,
+        'custid'    : inv.CustomerRef.value,
+        'custname'  : inv.CustomerRef.name,
+        'TotalAmt'  : inv.TotalAmt,
+        'Id'        : inv.Id,
+    }
+    return(bag)
+    
+
+def main():
+    global qbo_client
+    qbo_client = qu.open_qbo_client()
+
+
+    sqlbits = [];
+    sqlbits.append("TxnDate >= '{--starting}'".format(**arguments))
+    sqlbits.append("TxnDate < '{--ending}' ".format(**arguments))
+       
+    if not arguments['--custid'] is None:
+        custs = "CustomerRef in (" + ",".join("'{0}'".format(str(i)) for i in arguments['--custid']) + ")"
+        sqlbits.append(custs)
+
+    whereclause = " and ".join(sqlbits)
+        
+    
+    ilist = pd.DataFrame( [ invoice2row(inv) for inv in invoice_iterable(whereclause=whereclause)])
+    if (len(ilist) == 0 ) :
+        print("No matches for your query: \n---\n{0}\n---\n".format(whereclause))
+        sys.exit()
+
+    ilist['Id'] = ilist['Id'].astype(int)
+    ilist['custid'] = ilist['custid'].astype(int)
+    print(ilist)
+
+    bag = {}
+    bag['icount'] = len(ilist)
+    
+    if (arguments['--and-then-delete'] is True):
+        if ( arguments['--doit'] is True):
+            print("Batch deletion beginning.  Max batch is 100.")
+            invoices = Invoice.where(whereclause,qb=qbo_client)
+            results = batch_delete(invoices,qb=qbo_client)
+            
+            if len(results.faults) > 0:
+                print(results.faults)
+            else:
+                bag['scount'] = len(results.successes)
+                print("No faults returned; {scount} of {icount} removed.".format(**bag))
+
+                
+                
+
+                
+                
+                
+        else:
+            print("\nDeletion requested but not confirmed.  --doit if you mean it. \n")
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='Naval Fate 2.0')
+
+    debug = arguments['--debug']
+
+    if (arguments['--custid']):
+        arguments['--custid'] = [ int(x) for x in arguments['--custid'].split(",")]
+ 
+
+    if ( arguments['--starting'] == "the first" ) :
+        arguments['--starting'] = dateparser.parse("{0} days ago".format(dateparser.parse("today").day -1 )).date().isoformat()
+    else:
+        arguments['--starting'] = dateparser.parse(arguments['--starting']).date().isoformat()
+
+    arguments['--ending'] =  dateparser.parse(arguments['--ending']).date().isoformat()
+
+    
+        
+
+    if (debug):
+        qu.debug=True
+        hu.debug=True
+        print(arguments)
+
+    main()
